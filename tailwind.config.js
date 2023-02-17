e5a984243fd6ad2b55c73413ab72e7dea06f2b49
Sayed Ismail Ahmed <contact.ismailahmed@gmail.com>
2023-02-17 16:38:26 +0000

    image bugfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-"arcane": "../public/Images/image.jpg"
+"arcane": "url(/Images/image.jpg)"
       },
       animation: {
         'anim': ' flow  3s ease  infinite',
